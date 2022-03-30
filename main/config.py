13e64c7d27f6c0808dc33fe8ccb794f9668aca30
Ashitaka0721 <y.yasuda0502@gmail.com>
2022-03-30 09:57:48 +0900

    sqlalchemy, postgresの接続

--- main/config.py
+++ main/config.py
@@ -1,5 +1,10 @@
 import os
+import re
 
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or "sqlite:///test.db"
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SECRET_KEY="secret key"
+
+if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+# rest of connection code using the connection string `uri`
